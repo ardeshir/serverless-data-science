1854259ef93e40c77e960cf3daf9d368e45f88e4
ardeshir sepahsalar <ardeshir.org@gmail.com>
2019-05-17 11:04:50 -0500

    adding custom docker

--- train.py
+++ train.py
@@ -1,11 +1,16 @@
+try:
+  import unzip_requirements
+except ImportError:
+  pass
+
 import json
 
 def trainHandler(event, context):
-    body = json.loads(event.get('body'))
-    
+    body = json.loads( event.get('body') )
+
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
-    }   
-    
+    }
+
     return response
--- upload.py
+++ upload.py
@@ -1,7 +1,12 @@
+try:
+  import unzip_requirements
+except ImportError:
+  pass
+
 import json
 
 def uploadHandler(event, context):
-    body = json.loads(event.get('body'))
+    body = json.loads( event.get('body') )
 
     response = {
         "statusCode": 200,
